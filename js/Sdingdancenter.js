44c77409ea6a2772e9afc752185e31ff3d7ce428
zjrGitHub <939349242@qq.com>
2019-07-22 15:27:04 +0800

    商品类型

--- js/Sdingdancenter.js
+++ js/Sdingdancenter.js
@@ -2,7 +2,7 @@
 // layui.use(['laypage', 'layer'], function(){
 //     var laypage = layui.laypage
 //     ,layer = layui.layer;
-    
+
 //     //执行一个laypage实例
 //     laypage.render({
 //         elem: 'demo4'
@@ -33,107 +33,14 @@
 
 //点击按钮的样式
 $(document).ready(function () {
-    //商品类型两个字的按钮样式改变
+    //商品类型的按钮样式改变
     $(".shangpin1").click(function () {
         $(".shangpin1").toggleClass("product_types_selected1 product_types_unselect1");
         if ($(".shangpin1").hasClass("product_types_selected1")) {
-            $(".shangpin2").removeClass("product_types_selected1").addClass("product_types_unselect1");
-            $(".shangpin3").removeClass("product_types_selected1").addClass("product_types_unselect1");
-            $(".shangpin4").removeClass("product_types_selected1").addClass("product_types_unselect1");
-            $(".shangpin5").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin6").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin7").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin8").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin9").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin10").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin11").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin12").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin13").removeClass("product_types_select2").addClass("product_types_unselect2");
-            $(".shangpin14").removeClass("product_types_select2").addClass("product_types_unselect2");
+            $(".shangpin1~div").removeClass("product_types_selected1").addClass("product_types_unselect1");
+        }
+    })
 
-
-        }
-
-    })
-    $(".shangpin2").click(function () {
-        $(".shangpin2").toggleClass("product_types_selected1 product_types_unselect1")
-        if ($(".shangpin2").hasClass("product_types_selected1")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin3").click(function () {
-        $(".shangpin3").toggleClass("product_types_selected1 product_types_unselect1")
-        if ($(".shangpin3").hasClass("product_types_selected1")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin4").click(function () {
-        $(".shangpin4").toggleClass("product_types_selected1 product_types_unselect1")
-        if ($(".shangpin4").hasClass("product_types_selected1")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    //商品类型四个字的按钮样式改变
-    $(".shangpin5").click(function () {
-        $(".shangpin5").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin5").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin6").click(function () {
-        $(".shangpin6").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin6").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin7").click(function () {
-        $(".shangpin7").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin7").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin8").click(function () {
-        $(".shangpin8").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin7").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin9").click(function () {
-        $(".shangpin9").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin9").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin10").click(function () {
-        $(".shangpin10").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin10").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin11").click(function () {
-        $(".shangpin11").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin11").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin12").click(function () {
-        $(".shangpin12").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin12").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin13").click(function () {
-        $(".shangpin13").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin13").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
-    $(".shangpin14").click(function () {
-        $(".shangpin14").toggleClass("product_types_select2 product_types_unselect2")
-        if ($(".shangpin14").hasClass("product_types_select2")) {
-            $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
-        }
-    })
     //纯佣样式改变
     $(".style1").click(function () {
         $(".style1").toggleClass("order_types_selected1 product_types_unselect1")
@@ -187,22 +94,22 @@ $(document).ready(function () {
 
     //点击出现订单详细内容
     $(".zhuti2_1_1 .more").click(function () {
-          //用户选择非纯佣弹出二维码
+        //用户选择非纯佣弹出二维码
 
-        if( $(".style2").hasClass("order_types_selected2") ){
+        if ($(".style2").hasClass("order_types_selected2")) {
             $(".heisemo").show();
             $(".detail_1").hide();
             $(".detail_2").hide();
             $(".detail_3").show();
-            $(".heisemo").click(function(){
+            $(".heisemo").click(function () {
                 $(".heisemo").hide();
             })
-        }else if($(".style1").hasClass("order_types_selected1")){
+        } else if ($(".style1").hasClass("order_types_selected1")) {
             $(".heisemo").show();
             $(".detail_1").show();
             $(".detail_2").hide();
             $(".detail_3").hide();
-        }else{
+        } else {
             $(".heisemo").show();
         }
     })
@@ -230,8 +137,8 @@ $(document).ready(function () {
     })
 
     //跳转到创建订单页面
-    $(".create_dingdan").click(function(){
-        window.location.href="Schuangjiandingdan.html";
+    $(".create_dingdan").click(function () {
+        window.location.href = "Schuangjiandingdan.html";
     })
 
 })
--- js/Sorder_style.js
+++ js/Sorder_style.js
@@ -13,10 +13,128 @@ $.ajax({
     contentType: "application/json",
     data: JSON.stringify(pro_style_data),
     success: function (data) {
-        console.log('商品类型列表', data.data.dictList);
-        var list= data.data.dictList.dictType;
-        $.each(list,function(index,item){
-            
+        // console.log('商品类型列表', data.data.dictList);
+        var list = data.data.dictList;
+        $.each(list, function (index, item) {
+            $(".product_types").append(
+                '<div class="shangpin' + (index + 2)+' product_types_unselect1">' + item.dictType + '</div>'
+            );
+        })
+        //点击商品类型显示样式
+        $(".shangpin2").click(function () {
+            $(".shangpin2").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin2").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin3").click(function () {
+            $(".shangpin3").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin3").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin4").click(function () {
+            $(".shangpin4").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin4").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+
+        $(".shangpin5").click(function () {
+            $(".shangpin5").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin5").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin6").click(function () {
+            $(".shangpin6").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin6").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin7").click(function () {
+            $(".shangpin7").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin7").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin8").click(function () {
+            $(".shangpin8").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin7").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin9").click(function () {
+            $(".shangpin9").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin9").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin10").click(function () {
+            $(".shangpin10").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin10").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin11").click(function () {
+            $(".shangpin11").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin11").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin12").click(function () {
+            $(".shangpin12").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin12").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin13").click(function () {
+            $(".shangpin13").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin13").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin14").click(function () {
+            $(".shangpin14").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin14").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin15").click(function () {
+            $(".shangpin15").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin15").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin16").click(function () {
+            $(".shangpin16").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin16").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin17").click(function () {
+            $(".shangpin17").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin17").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin18").click(function () {
+            $(".shangpin18").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin18").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin19").click(function () {
+            $(".shangpin19").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin19").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
+        })
+        $(".shangpin20").click(function () {
+            $(".shangpin20").toggleClass("product_types_selected1 product_types_unselect1")
+            if ($(".shangpin20").hasClass("product_types_selected1")) {
+                $(".shangpin1").removeClass("product_types_selected1").addClass("product_types_unselect1");
+            }
         })
 
     }
